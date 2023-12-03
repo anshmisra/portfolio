3eeb98a1432273a46a4e75bed82d0f9fe81d55fc
anshmisra <151787276+anshmisra@users.noreply.github.com>
2023-12-03 22:48:23 +0000

    modified:   __pycache__/info.cpython-311.pyc 	new file:   cover.png 	new file:   cvs.png 	new file:   emory.jpeg 	new file:   gtsna.jpeg 	modified:   info.py 	new file:   medlife.png 	new file:   qurbani.png 	new file:   research.jpg 	new file:   yellowjacket.png

--- info.py
+++ info.py
@@ -2,7 +2,7 @@
 #This File will contain the information to be displayed in your portfolio
 
 #CHANGE BELOW
-profile_picture = "images/cover.png"
+profile_picture = "cover.png"
 about_me = "I'm Ansh Misra. I am a first-year Neuroscience student at the Georgia Institute of Technology with an interest in cancer biology, psychology, and computer science. I am seeking opportunites in the medical industry, hoping to make a positive impact on society and all people I meet. I strive to be an active member in all aspects of the college community including academics, arts, and athletics."
 
 
@@ -31,14 +31,14 @@ course_data = {
     "skills":["Basic cellular function, Genetics, Ecology", "Differentiation of equations, Application of Derivatives", "Integration of equations, Application of Integrals", "Solving system of equations, Applications to linear systems", "Linear approximation, Taylor's theorems, Multiple integration, Vector analysis", "Atomic structure, Properties of matter, Thermodynamics, Laboratory skills", "Coding in Python, Web Design, Basic software development", "Psychological approaches and theories, Research methods"],
     }
 experience_data = {
-    "Pharmacy Technician at CVS Pharmacy": (["- Dispense and distribute medication to customers", "- Answering and fulfilling prescription orders online and through the phone"],"images/cvs.png"),
-    "Volunteer at Emory Midtown Winship Cancer Institute":(["- Offering support to patients and their families to improve their care experience", "- Signing in and escorting patients to their rooms"],"images/emory.jpeg"),
-    "Researcher in Prulitsky Biomechanics and Motor Control Lab":(["- Analyzing animal motorcontrol and gait data", "- Creating digital model for animal motor systems"],"images/research.jpg")
+    "Pharmacy Technician at CVS Pharmacy": (["- Dispense and distribute medication to customers", "- Answering and fulfilling prescription orders online and through the phone"],"cvs.png"),
+    "Volunteer at Emory Midtown Winship Cancer Institute":(["- Offering support to patients and their families to improve their care experience", "- Signing in and escorting patients to their rooms"],"emory.jpeg"),
+    "Researcher in Prulitsky Biomechanics and Motor Control Lab":(["- Analyzing animal motorcontrol and gait data", "- Creating digital model for animal motor systems"],"research.jpg")
 
 }
 
 projects_data = {
-    "Yellow Jacket Lifts": ("Application that provides full workouts targetting specific muscle groups for college students to accomplish their fitness goals. Most college students are busy and short on time, so the application designs a workout with exercises that fits the student's schedule.", "images/yellowjacket.png", "https://yellowjacketlifts.streamlit.app")
+    "Yellow Jacket Lifts": ("Application that provides full workouts targetting specific muscle groups for college students to accomplish their fitness goals. Most college students are busy and short on time, so the application designs a workout with exercises that fits the student's schedule.", "yellowjacket.png", "https://yellowjacketlifts.streamlit.app")
 }
 
 programming_data = {
@@ -70,11 +70,11 @@ spoken_data = {
     "Spanish": "Conversant",
 }
 leadership_data = {
-    "Finance Officer for GT MEDLIFE": (["- Aiding fundraising director in collecting funds for the club", "- Planning fundraising events for the club and the GT community", "- Tracking internal finances"],"images/medlife.png"),
+    "Finance Officer for GT MEDLIFE": (["- Aiding fundraising director in collecting funds for the club", "- Planning fundraising events for the club and the GT community", "- Tracking internal finances"],"medlife.png"),
 
 }
 activity_data={
-    "Georgia Tech MEDLIFE": (["- Raising money for healthcare, education, and medical supplies for developing countries", "- Creating awareness for improved healthcare for people in developing countries", "- Sorting medical supplies at MEDSHARE distribution centers"], "images/medlife.png"),
-    "Student Neuroscience Association": (["- Providing opportunities for undergraduate Neuroscience students", "- Sharing information on current news in Neuroscience", "- Raising awareness for students' concerns of current course curriculum and student experience"], "images/gtsna.jpeg"),
-    "Georgia Tech Qurbani": (["- Spreading the love for Indian culture and music through dance", "- Traveling throughout the country taking part in various dance competitions"], "images/qurbani.png")
+    "Georgia Tech MEDLIFE": (["- Raising money for healthcare, education, and medical supplies for developing countries", "- Creating awareness for improved healthcare for people in developing countries", "- Sorting medical supplies at MEDSHARE distribution centers"], "medlife.png"),
+    "Student Neuroscience Association": (["- Providing opportunities for undergraduate Neuroscience students", "- Sharing information on current news in Neuroscience", "- Raising awareness for students' concerns of current course curriculum and student experience"], "gtsna.jpeg"),
+    "Georgia Tech Qurbani": (["- Spreading the love for Indian culture and music through dance", "- Traveling throughout the country taking part in various dance competitions"], "qurbani.png")
 }
